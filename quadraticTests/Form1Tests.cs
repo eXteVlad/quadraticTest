033a660696068124bf5c2547f4bc0544380153f4
eXteVlad <extevlad@yandex.ru>
2017-05-17 17:24:29 +0400

    Added test project for quadratic

---
+++ quadraticTests/Form1Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace quadraticTests
+{
+    [TestClass]
+    public class Form1Tests
+    {
+        [TestMethod]
+        public void Disc_321_1()
+        {
+            int a = 2;
+            int b = 3;
+            int c = 1;
+            int expected = 1;
+
+            quadratic.Form1 f = new quadratic.Form1();
+            double actual = f.Disc(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Disc_111_neg3()
+        {
+            int a = 1;
+            int b = 1;
+            int c = 1;
+            int expected = -3;
+
+            quadratic.Form1 f = new quadratic.Form1();
+            double actual = f.Disc(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
